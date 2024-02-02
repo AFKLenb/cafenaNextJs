ca51c40aae94a49b59c0d14d50bc1a4f7ba25d36
AFKLenb <markermma@gmail.com>
2024-02-02 17:11:21 +0300

    инициализировали проект frontend 1

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,22 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      '2xl': {'max': '1400px'},
+      // => max-width: 1400px { ... }
+      
+      'xl': {'max': '1200px'},
+      // => max-width: 1200px { ... }
+      
+      'lg': {'max': '1024px'},
+      // => max-width: 1024px { ... }
+      
+      'md': {'max': '768px'},
+      // => max-width: 768px { ... }
+      
+      'sm': {'max': '564px'},
+      // => max-width: 564px { ... }
+      },
   },
   plugins: [],
 };
